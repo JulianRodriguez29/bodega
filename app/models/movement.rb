fb66d4423d38006869b41961702a8eab2b5fc818
JulianRodriguez29 <josejulian_rodriguez@ucol.mx>
2022-11-26 21:48:41 -0600

    Creating movement logic

---
+++ app/models/movement.rb
@@ -0,0 +1,13 @@
+class Movement < ApplicationRecord
+  belongs_to :product
+  MovementTypes = {add: 0, remove: 1}
+  validates :quantity, presence: true, numericality: true
+
+
+  def self.get_movement_type
+    { 
+      'Agregar' => MovementTypes[:add],
+      'Quitar' => MovementTypes[:remove]
+     }
+  end
+end
